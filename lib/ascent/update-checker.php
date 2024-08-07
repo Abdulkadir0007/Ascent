24b685300fe24481f91ad277de9aae69b33bdd7f
Abdul Kadir <mabdulkadir5g@gmail.com>
2024-08-08 00:23:03 +0600

    Add initial theme files

--- lib/ascent/update-checker.php
+++ lib/ascent/update-checker.php
@@ -2,16 +2,21 @@
 /**
  * Theme Update Checker
  */
-// Include the update checker
-use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
-// Include the library's main file
-require_once get_template_directory() . '/lib/plugin-update-checker/plugin-update-checker.php';
 
-$myUpdateChecker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
-    'https://github.com/Abdulkadir070/Ascent',
-    get_template_directory(),
+// Include the library (remember to change the path).
+require get_template_directory() . '/lib/plugin-update-checker/plugin-update-checker.php';
+
+// Instantiate the update checker.
+$updateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/Abdulkadir070/Ascent/',
+    __FILE__,
     'ascent'
 );
-//Optional: Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('main');
+
+// Optional: Set the branch that contains the stable release.
+$updateChecker->setBranch('main');
+
+// Optional: If you're using a private repository, specify the access token like this:
+//$updateChecker->setAuthentication('your-github-token');
 ?>
+
