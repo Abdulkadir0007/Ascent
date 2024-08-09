84916c0d7a5a2c2f6b0999cf3c5363c706133cde
Abdul Kadir <mabdulkadir5g@gmail.com>
2024-08-09 14:32:33 +0600

    Update

--- lib/ascent/update-checker.php
+++ lib/ascent/update-checker.php
@@ -3,16 +3,16 @@
  * Theme Update Checker
  */
 
+ require get_template_directory() . '/lib/plugin-update-checker/plugin-update-checker.php';
 // Include the library (remember to change the path).
-require get_template_directory() . '/lib/plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-// Instantiate the update checker.
-$updateChecker = PucFactory::buildUpdateChecker(
-    'https://raw.githubusercontent.com/Abdulkadir0007/Ascent/main/update-info.json',
-    get_template_directory() . '/style.css', // Path to a file in your theme
-    'ascent'
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://raw.githubusercontent.com/Abdulkadir0007/Ascent/main/update-info.json',
+	__FILE__,
+	'ascent'
 );
+
 
 // Optional: If you're using a private repository, specify the access token like this:
 //$updateChecker->setAuthentication('your-github-token');
